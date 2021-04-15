6e2d5e25d0e450239dfe35aeb9afc0cafed5bf63
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-04-15 09:20:52 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-let pokemonList = [{name: "Pikachu", height: 1.04, type: "Electric"},
+let pokemonList = [{name: "Pikachu", height: 1.04, type: ["Electric"]},
 {name: "Charizard", height: 5.07, type: ["Fire", "Flying"]},
 {name: "Gengar", height: 4.11, type: ["Ghost", "Poison"]}];
