4989e6f118d862535f6bebac7170c9a11e8d0d8f
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-04-16 15:15:09 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-let pokemonList = [{name: "Pikachu", height: 1.04, type: ["Electric"]},
-{name: "Charizard", height: 5.07, type: ["Fire", "Flying"]},
-{name: "Gengar", height: 4.11, type: ["Ghost", "Poison"]}];
+// list 3 Pokemons in an Array to include name, height and type
+let pokemonList = [
+{ name: 'Pikachu', height: 1.04, type: ['Electric'] },
+{ name: 'Charizard', height: 5.07, type: ['Fire', 'Flying'] },
+{ name: 'Gengar', height: 4.11, type: ['Ghost', 'Poison'] },
+];
+
+//Print on document the name and height of each Pokemon. Highlight also special Pokemon
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 5)
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') '
+  + ' - Wow, that\'s big! ');
+  else if (pokemonList[i].height < 5)
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+}
