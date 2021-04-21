551aceed4a7564721515e2044ddcb7225ee60591
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-04-21 23:17:52 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,6 @@ var pokemonRepository = (function ()
 { name: 'Charizard', height: 5.07, type: ['Fire', 'Flying'] },
 { name: 'Gengar', height: 4.11, type: ['Ghost', 'Poison'] },
 ];
-
-  pokemonList.forEach(function (pokemon) {
-      if (pokemon.height > 5)
-  document.write('<div>' + pokemon.name + ' (height: ' + pokemon.height + ') '
-  + ' - Wow, that\'s big! </div>');
-      else if (pokemon.height < 5)
-  document.write('<div>' + pokemon.name + ' (height: ' + pokemon.height + ') </div>');
-    });
-
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
@@ -26,3 +17,11 @@ var pokemonRepository = (function ()
     getAll: getAll,
   };
 })();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    if (pokemon.height > 5)
+document.write('<div>' + pokemon.name + ' (height: ' + pokemon.height + ') '
++ ' - Wow, that\'s big! </div>');
+    else if (pokemon.height < 5)
+document.write('<div>' + pokemon.name + ' (height: ' + pokemon.height + ') </div>');
+  });
