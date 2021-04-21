c6bf42ecf7e1a98cfbdba9a4495deb3e9ef1700b
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-04-21 22:28:00 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
-// list 3 Pokemons in an Array to include name, height and type
-let pokemonList = [
-{ name: 'Pikachu', height: 1.04, type: ['Electric'] },
+var pokemonRepository = (function ()
+{
+  var pokemonList = [{ name: 'Pikachu', height: 1.04, type: ['Electric'] },
 { name: 'Charizard', height: 5.07, type: ['Fire', 'Flying'] },
 { name: 'Gengar', height: 4.11, type: ['Ghost', 'Poison'] },
 ];
 
-//Print on document the name and height of each Pokemon. Highlight also special Pokemon
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 5)
-  document.write('<div>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') '
-  + ' - Wow, that\'s big! </div>');
-  else if (pokemonList[i].height < 5)
-  document.write('<div>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') </div>');
-}
+  pokemonList.forEach(function (pokemon) {
+      if (pokemon.height > 5)
+  console.log(pokemon.name + ' (height: ' + pokemon.height + ') '
+  + ' - Wow, that\'s big!');
+      else if (pokemon.height < 5)
+  console.log(pokemon.name + ' (height: ' + pokemon.height + ') ');
+    });
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
