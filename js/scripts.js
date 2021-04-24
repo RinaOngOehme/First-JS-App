404db6458e39108c4ad714c2f4684fb639e93a05
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-04-24 13:11:43 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -44,10 +44,7 @@ let pokemonRepository = (function () {
     button.addEventListener("click", function (showDetails) {console.log(pokemon);});
   }
 
-  function showDetails(pokemon) {
-    console.log(pokemon);
-  }
-
+  
   return {
     add: add,
     getAll: getAll,
