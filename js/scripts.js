d0399d1e4f72ee0afd8ad6181c47ad3e373a893f
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-04-26 21:30:41 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,21 @@
 let pokemonRepository = (function () {
-  let repository = [{
-      name: "Pikachu",
-      height: 1.04,
-      types: ["Electric"],
-    },
-    {
-      name: "Charizard",
-      height: 5.07,
-      types: ["Fire", "Flying"],
-    },
-    {
-      name: "Gengar",
-      height: 4.11,
-      types: ["Ghost", "Poison"],
-    },
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
       "name" in pokemon &&
-      "height" in pokemon &&
-      "types" in pokemon
-    ) {
-      repository.push(pokemon);
+      "detailsUrl" in pokemon
+       ) {
+      pokemonList.push(pokemon);
     } else {
       return ("pokemon is not correct");
     }
   }
 
   function getAll() {
-    return repository;
+    return pokemonList;
   }
 
   function addListItem(pokemon) {
@@ -41,28 +26,58 @@ let pokemonRepository = (function () {
     button.classList.add("button-class");
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
-    button.addEventListener("click", function () {showDetails(pokemon);});
+    button.addEventListener("click", function (event) {showDetails(pokemon);});
   }
 
-function showDetails(pokemon){
-  console.log(pokemon)
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url,
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
   }
 
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails,
   };
 })();
 
-pokemonRepository.add({
-  name: "Arcanine",
-  height: 6.03,
-  types: ["Fire"],
-});
-
-pokemonRepository.getAll();
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  // Now the data is loaded
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
